70d8085f0c42bc17ba1f1629db12f24e26c6c41a
francino97 <edmilsonfrancino@gmail.com>
2020-04-10 22:34:27 -0300

    NewOrder-AmountStorage

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import NewProduct from './pages/NewProduct';
+import NewOrder from './pages/NewOrder';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/novo_produto" exact component={NewProduct} />
+        <Route path="/nova_compra" exact component={NewOrder} />
       </Switch>
     </BrowserRouter>
   );
